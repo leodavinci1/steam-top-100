c7874d3022ed3f9d4fe0ae4b451ce27d2fc86e30
Leonardo Cunha <leonardo.cunha@objectedge.com>
2021-03-15 02:10:54 -0300

    Add some charts

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Chart from "react-apexcharts";
+import jsonData from "../../data/top100.json";
+
 
 const Home = () => {
+  
+const initialOptions = {
+  plotOptions: {
+    treemap: {
+      distributed: true
+    }
+  },
+  events: {
+    click: function(event, chartContext, config) {
+      console.log(chartContext);
+    }
+  }
+}
+  console.log(jsonData);
+  const [options, setOptions] = useState(initialOptions);
   return (
     <div className="about-container">
-      <h2>Boilerplate</h2>
+      {/* <Chart
+              options={options}
+              series={[]}
+              type="bar"
+              width="500"
+            /> */}
     </div>
   );
 }
